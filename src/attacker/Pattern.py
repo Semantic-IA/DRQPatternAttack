8d3e42c92a4172e1e569215893679aecc9589d16
Max Maass <0maass@informatik.uni-hamburg.de>
2013-04-04 13:05:10 +0200

    +Working distinguishable first block range query generator. +working DFBPatternAttack. +Working conversion of datastructures to sets.

--- src/attacker/Pattern.py
+++ src/attacker/Pattern.py
@@ -14,12 +14,13 @@ class patternV1():
         """Attack a given Range Query
         
         @param rq: A Range Query, as returned by generate.DRQ
-        @return: TBD"""
+        @return: list of possible results
+        """
         # TODO: Think about return format
         res = []
         for key in DB.PATTERNS.keys():
             inter = intersection(rq,DB.PATTERNS[key])
-            if len(inter) == len(DB.PATTERNS[key]) and not len(rq) % len(inter):
+            if len(inter) == len(DB.PATTERNS[key]):
                 res.append(key)
         return res
     
@@ -27,10 +28,11 @@ class DFBPattern():
     """Distinguishable First Block Pattern Attack"""
     
     def attack(self,fb,rq):
-        """Attack a given Range  Query with a distinguishable first block
+        """Attack a given Range Query with a distinguishable first block
         
         @param fb: The first block, as set
         @param rq: The remaining range query, as set
+        @return: List of possible results
         """
         res = []
         rq.update(fb)
@@ -39,5 +41,6 @@ class DFBPattern():
                 if DB.PATTERNS[key] <= rq:
                     res.append(key)
         return res
-# Unterscheidbare Blöcke
-# Erster Block unterscheidbar
+
+### Moegliche weitere Annahmen:
+# Komplett unterscheidbare Bloecke
--- src/generate/DRQ.py
+++ src/generate/DRQ.py
@@ -12,12 +12,27 @@ def generateDRQFor(domain):
     """Generate a Range Query for a given domain name.
     
     @param domain: The domain name for which a range query should be constructed
-    @return: A set of queries"""
-    # TODO: Verify functionality after change to set
-    # TODO: Add check that the size requirements are met after transition to set
+    @return: A set of queries
+    """
     # TODO: Check this function after defining the exact RQ Algorithm
     query = []
     for subquery in DB.PATTERNS[domain]:
         query.append(subquery)
         query.extend(DB.chooseRandomHosts(Config.RQSIZE-1))
     return set(query)
+
+def generateDDRQFor(domain):
+    """Generate a Range Query with a distinguishable first query block.
+    
+    @param domain: The domain name for which a range query should be constructed
+    @return: A tuple of two sets, the first containing the first query block, the second containing the remaining queries
+    """
+    head = set()    # First Set of Queries
+    block = set()   # Remaining Queries
+    head.add(domain)
+    head.update(DB.chooseRandomHosts(Config.RQSIZE-1))
+    for subquery in DB.PATTERNS[domain]:
+        if subquery != domain:
+            block.add(subquery)
+            block.update(DB.chooseRandomHosts(Config.RQSIZE-1))
+    return (head, block)
