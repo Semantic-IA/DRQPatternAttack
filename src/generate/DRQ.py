54710df2044fccde4b1dfef7ac12672ca6b8e783
Max Maass <max@velcommuta.de>
2013-09-25 17:18:51 +0200

    Removed a few double copy statements that were just eating up CPU time.

--- src/generate/DRQ.py
+++ src/generate/DRQ.py
@@ -77,7 +77,7 @@ class PatternRangeQuery(object):
             hosts.update(set(DB.getRandomHostsByPatternLengthB(pattern_length, Config.RQSIZE-1, hosts)))
             pattern_copy = {}
             for host in hosts:
-                pattern_copy[host] = DB.getPatternForHost(host).copy()
+                pattern_copy[host] = DB.getPatternForHost(host)
                 pattern_copy[host].remove(host)  # TODO: Ugly code. Any way to make this less horrible?
                 block[0].add(host)
             for i in range(1, pattern_length, 1):
@@ -89,28 +89,29 @@ class PatternRangeQuery(object):
             padding = []
             for i in range(num_of_needed_patterns):
                 pad1_len, pad2_len = self.getRandomNumbersWithSum(2, pattern_length)
+                # TODO: This may livelock on very small data sets. Fix this.
                 while ((DB.getNumberOfHostsWithPatternLengthB(pad1_len, block[0]) == 0)
                         or (DB.getNumberOfHostsWithPatternLength(pad2_len) == 0)):
                     pad1_len, pad2_len = self.getRandomNumbersWithSum(2, pattern_length)
                 pad1_host = DB.getRandomHostsByPatternLengthB(pad1_len, 1, block[0])[0]
-                pad1_pattern = DB.getPatternForHost(pad1_host).copy()
+                pad1_pattern = DB.getPatternForHost(pad1_host)
                 pad1_pattern.remove(pad1_host)
                 block[0].add(pad1_host)
                 padding.append([pad1_host])
                 for host in pad1_pattern:
                     padding[i].append(host)
                 pad2_host = DB.getRandomHostsByPatternLength(pad2_len, 1)[0]
-                pad2_pattern = DB.getPatternForHost(pad2_host).copy()
+                pad2_pattern = DB.getPatternForHost(pad2_host)
                 pad2_pattern.remove(pad2_host)
                 padding[i].append(pad2_host)
                 for host in pad2_pattern:
                     padding[i].append(host)
             pattern_copy = {}
             block[0].add(domain)
-            pattern_copy[domain] = DB.getPatternForHost(domain).copy()
+            pattern_copy[domain] = DB.getPatternForHost(domain)
             pattern_copy[domain].remove(domain)
             for element in DB.getRandomHostsByPatternLengthB(pattern_length, num_of_available_patterns, block[0]):
-                pattern_copy[element] = DB.getPatternForHost(element).copy()
+                pattern_copy[element] = DB.getPatternForHost(element)
                 pattern_copy[element].remove(element)
                 block[0].add(element)
             for i in range(1, pattern_length, 1):
